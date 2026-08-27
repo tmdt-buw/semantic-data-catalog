1ae26e26fc19bee120e87e2ae3ec353b16f0e4f3
Florian Hölken <hoelken@uni-wuppertal.de>
2026-08-27 22:26:15 +0200

    chore: sync catalog runtime version

--- frontend/public/env.js
+++ frontend/public/env.js
@@ -1,6 +1,6 @@
 window._env_ = {
   "REACT_APP_REDIRECT_URL": "",
-  "REACT_APP_VERSION": "0.8.64",
+  "REACT_APP_VERSION": "0.8.65",
   "STATISTICS_ENABLED": true,
   "STATISTICS_POD_BASE_URL": "https://solid-community-server.tmdt.info/solidtestpod/statistics/",
   "STATISTICS_EVENTS_URL": "https://solid-community-server.tmdt.info/solidtestpod/statistics/events/catalog-instances/test/downloads/",
--- frontend/src/version.js
+++ frontend/src/version.js
@@ -1,1 +1,1 @@
-export const appVersion = "0.8.64";
+export const appVersion = "0.8.65";
